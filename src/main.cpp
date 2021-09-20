02bddbce49614a5f25aeeeea96f90ba7e7899252
Alexander Valiullin <phoenix-asv@yandex.ru>
2021-09-20 16:54:23 +0300

    task01: 3

--- src/main.cpp
+++ src/main.cpp
@@ -34,6 +34,9 @@ void reportError(cl_int err, const std::string &filename, int line)
 #define OCL_SAFE_CALL(expr) reportError(expr, __FILE__, __LINE__)
 
 
+cl_int ret_code;
+
+
 int main()
 {
     // Пытаемся слинковаться с символами OpenCL API в runtime (через библиотеку clew)
@@ -94,7 +97,6 @@ int main()
     // код по переданному аргументом errcode_ret указателю)
     // И хорошо бы сразу добавить в конце clReleaseContext (да, не очень RAII, но это лишь пример)
 
-    cl_int ret_code;
     cl_context context = clCreateContext(nullptr, 1, &chosen_device, nullptr, nullptr, &ret_code);
     OCL_SAFE_CALL(ret_code);
 
@@ -102,6 +104,9 @@ int main()
     // См. документацию https://www.khronos.org/registry/OpenCL/sdk/1.2/docs/man/xhtml/ -> OpenCL Runtime -> Runtime APIs -> Command Queues -> clCreateCommandQueue
     // Убедитесь, что в соответствии с документацией вы создали in-order очередь задач
     // И хорошо бы сразу добавить в конце clReleaseQueue (не забывайте освобождать ресурсы)
+
+    cl_command_queue command_queue = clCreateCommandQueue(context, chosen_device, 0, &ret_code);
+    OCL_SAFE_CALL(ret_code);
 
     unsigned int n = 1000*1000;
     // Создаем два массива псевдослучайных данных для сложения и массив для будущего хранения результата
@@ -222,6 +227,7 @@ int main()
 //        }
 //    }
 
+    OCL_SAFE_CALL(clReleaseCommandQueue(command_queue));
     OCL_SAFE_CALL(clReleaseContext(context));
 
     return 0;
