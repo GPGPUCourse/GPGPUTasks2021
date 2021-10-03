5937a7fcabc38918a61d581b78557d3e7dafa3f3
Maria Malysheva <mn.malysheva@mail.ru>
2021-10-03 23:00:52 +0300

    task03

--- src/cl/sum.cl
+++ src/cl/sum.cl
@@ -1,1 +1,70 @@
-// TODO
+#ifdef __CLION_IDE__
+#include <libgpu/opencl/cl/clion_defines.cl>
+#endif
+
+#line 6
+
+#define WORK_GROUP_SIZE 256
+
+__kernel void sum(__global const unsigned int* xs,
+                  __global       unsigned int* sum,
+                  unsigned int n,
+                  int last_iteration)
+{
+    const unsigned int global_id = get_global_id(0);
+    const unsigned int local_id = get_local_id(0);
+    const unsigned int group_id = get_group_id(0);
+
+    __local unsigned int local_xs[WORK_GROUP_SIZE];
+
+    if (global_id < n) {
+        local_xs[local_id] = xs[global_id];
+    } else {
+        local_xs[local_id] = 0;
+    }
+
+    barrier(CLK_LOCAL_MEM_FENCE);
+    if (local_id < 128) {
+        local_xs[local_id] += local_xs[local_id + 128];
+    }
+
+    barrier(CLK_LOCAL_MEM_FENCE);
+    if (local_id < 64) {
+        local_xs[local_id] += local_xs[local_id + 64];
+    }
+
+    barrier(CLK_LOCAL_MEM_FENCE);
+    if (local_id < 32) {
+        local_xs[local_id] += local_xs[local_id + 32];
+    }
+
+    barrier(CLK_LOCAL_MEM_FENCE);
+    if (local_id < 16) {
+        local_xs[local_id] += local_xs[local_id + 16];
+    }
+
+    barrier(CLK_LOCAL_MEM_FENCE);
+    if (local_id < 8) {
+        local_xs[local_id] += local_xs[local_id + 8];
+    }
+
+    barrier(CLK_LOCAL_MEM_FENCE);
+    if (local_id < 4) {
+        local_xs[local_id] += local_xs[local_id + 4];
+    }
+
+    barrier(CLK_LOCAL_MEM_FENCE);
+    if (local_id < 2) {
+        local_xs[local_id] += local_xs[local_id + 2];
+    }
+
+    barrier(CLK_LOCAL_MEM_FENCE);
+    if (local_id == 0) {
+        local_xs[0] += local_xs[1];
+        if (last_iteration) {
+            atomic_add(sum, local_xs[0]);
+        } else {
+            sum[group_id] = local_xs[0];
+        }
+    }
+}
--- src/main_sum.cpp
+++ src/main_sum.cpp
@@ -1,6 +1,10 @@
 #include <libutils/misc.h>
 #include <libutils/timer.h>
 #include <libutils/fast_random.h>
+#include <libgpu/context.h>
+#include <libgpu/shared_device_buffer.h>
+
+#include "cl/sum_cl.h"
 
 
 template<typename T>
@@ -59,6 +63,58 @@ int main(int argc, char **argv)
 
     {
         // TODO: implement on OpenCL
-        // gpu::Device device = gpu::chooseGPUDevice(argc, argv);
+        gpu::Device device = gpu::chooseGPUDevice(argc, argv);
+
+        gpu::Context context;
+        context.init(device.device_id_opencl);
+        context.activate();
+
+        unsigned int group_size = 256;
+
+        gpu::gpu_mem_32u xs_gpu, ys_gpu, zs_gpu;
+
+        xs_gpu.resizeN(n);
+        ys_gpu.resizeN((n + group_size - 1) / group_size);
+        zs_gpu.resizeN((n + group_size - 1) / group_size);
+
+        xs_gpu.writeN(as.data(), n);
+
+        ocl::Kernel kernel(sum_kernel, sum_kernel_length, "sum");
+        kernel.compile();
+
+        timer t;
+        for (int iter = 0; iter < benchmarkingIters; ++iter) {
+            unsigned int sum = 0;
+            gpu::gpu_mem_32u sum_gpu;
+            sum_gpu.resizeN(1);
+            sum_gpu.writeN(&sum , 1);
+
+            gpu::gpu_mem_32u* xs_ptr = &xs_gpu;
+            gpu::gpu_mem_32u* sum_ptr = &ys_gpu;
+
+            for (unsigned int current_size = n; ; current_size = (current_size + group_size - 1) / group_size) {
+                unsigned int work_size = (current_size + group_size - 1) / group_size * group_size;
+
+                if (current_size < group_size * group_size) {
+                    kernel.exec(gpu::WorkSize(group_size, work_size), *xs_ptr, sum_gpu, current_size, 1);
+                    break;
+                }
+
+                kernel.exec(gpu::WorkSize(group_size, work_size), *xs_ptr, *sum_ptr, current_size, 0);
+                if (current_size == n) {
+                    xs_ptr = sum_ptr;
+                    sum_ptr = &zs_gpu;
+                } else {
+                    std::swap(xs_ptr, sum_ptr);
+                }
+            }
+
+            sum_gpu.readN(&sum, 1);
+
+            EXPECT_THE_SAME(reference_sum, sum, "GPU result should be consistent!");
+            t.nextLap();
+        }
+        std::cout << "GPU:     " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
+        std::cout << "GPU:     " << (n/1000.0/1000.0) / t.lapAvg() << " millions/s" << std::endl;
     }
 }
