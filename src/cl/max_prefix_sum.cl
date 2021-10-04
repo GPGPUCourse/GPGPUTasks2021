eea2490a95506a768e2b570173c751f845dbb4f6
kononovk <kononov.nikolay.nk1@gmail.com>
2021-10-04 23:00:03 +0300

    done prefix some

--- src/cl/max_prefix_sum.cl
+++ src/cl/max_prefix_sum.cl
@@ -1,1 +1,67 @@
-// TODO
+#define WORK_GROUP_SIZE 128
+
+__kernel void max_prefix_sum(__global const int *numbers, __global int *result, __global int *max_pref_sum)
+{
+    int localId = get_local_id(0);
+    int globalId = get_global_id(0);
+    int groupSize = WORK_GROUP_SIZE;
+    int groupNum = get_global_id(0) / groupSize;
+    int numGroups = get_global_size(0) / WORK_GROUP_SIZE;
+    __local int lresult[WORK_GROUP_SIZE];
+    lresult[localId] = numbers[globalId];
+
+    int offset = 2;
+    for (unsigned int iteration = groupSize / 2; iteration > 0; iteration /= 2) {
+        barrier(CLK_LOCAL_MEM_FENCE);
+        if ((localId + 1) % offset == 0) {
+            lresult[localId] += lresult[localId - (offset / 2)];
+        }
+        offset *= 2;
+    }
+    barrier(CLK_LOCAL_MEM_FENCE);
+
+    if (localId == 0) {
+        lresult[WORK_GROUP_SIZE - 1] = 0;
+    }
+
+    for (unsigned int d = 1; d < groupSize; d *= 2) {
+        offset /= 2;
+        barrier(CLK_LOCAL_MEM_FENCE);
+        if ((localId + 1) % offset == 0) {
+            int old = lresult[localId];
+            lresult[localId] += lresult[localId - (offset / 2)];
+            lresult[localId - (offset / 2)] = old;
+        }
+    }
+
+    barrier(CLK_LOCAL_MEM_FENCE);
+
+    result[globalId] = lresult[localId] + numbers[globalId];
+
+
+    barrier(CLK_LOCAL_MEM_FENCE);
+
+    __local int lres[WORK_GROUP_SIZE];
+
+    lres[localId] = result[globalId];
+
+    // very slow part of code
+    for (int i = 0; i < groupNum; ++i) {
+        lres[localId] += result[(i + 1) * groupSize - 1];
+    }
+
+    // barrier(CLK_LOCAL_MEM_FENCE);
+    // find maximum of local array
+    for (unsigned int iteration = groupSize / 2; iteration > 0; iteration /= 2) {
+        barrier(CLK_LOCAL_MEM_FENCE);
+
+        if (localId < iteration) {
+            lres[localId] =
+                lres[localId] > lres[localId + iteration] ? lres[localId] : lres[localId + iteration];
+        }
+    }
+
+    if (localId == 0) {
+        atomic_max(max_pref_sum, lres[0]);
+    }
+}
--- src/main_max_prefix_sum.cpp
+++ src/main_max_prefix_sum.cpp
@@ -1,6 +1,10 @@
 #include <libutils/misc.h>
 #include <libutils/timer.h>
 #include <libutils/fast_random.h>
+#include <libgpu/context.h>
+#include <libgpu/shared_device_buffer.h>
+
+#include "cl/max_prefix_sum_cl.h"
 
 
 template<typename T>
@@ -17,13 +21,23 @@ void raiseFail(const T &a, const T &b, std::string message, std::string filename
 
 int main(int argc, char **argv)
 {
-    int benchmarkingIters = 10;
-    int max_n = (1 << 24);
+    int benchmarkingIters = 1;
+    int max_n = (1 << 22);
 
-    for (int n = 2; n <= max_n; n *= 2) {
+    gpu::Device device = gpu::chooseGPUDevice(argc, argv);
+    gpu::Context context;
+    context.init(device.device_id_opencl);
+    context.activate();
+    ocl::Kernel prefix_sum(max_prefix_sum_kernel, max_prefix_sum_kernel_length, "max_prefix_sum");
+    prefix_sum.compile();
+
+    const unsigned int workGroupSize = 128;
+
+    for (int n = 2; n <= 16777216; n *= 2) {
         std::cout << "______________________________________________" << std::endl;
         int values_range = std::min(1023, std::numeric_limits<int>::max() / n);
         std::cout << "n=" << n << " values in range: [" << (-values_range) << "; " << values_range << "]" << std::endl;
+        unsigned int globalWorkSize = (n + workGroupSize - 1) / workGroupSize * workGroupSize;
 
         std::vector<int> as(n, 0);
         FastRandom r(n);
@@ -71,7 +85,27 @@ int main(int argc, char **argv)
         }
 
         {
-            // TODO: implement on OpenCL
+            gpu::gpu_mem_32i numbers_gpu = gpu::gpu_mem_32i::createN(n);
+            numbers_gpu.writeN(as.data(), n);
+
+            gpu::gpu_mem_32i max_sum_gpu = gpu::gpu_mem_32i::createN(1);
+            int a = 0;
+            max_sum_gpu.writeN(&a, 1);
+
+            gpu::gpu_mem_32i result_gpu = gpu::gpu_mem_32i::createN(n);
+
+            timer t;
+            std::vector<int> result(n, 0);
+            for (int iter = 0; iter < benchmarkingIters; ++iter) {
+                int max_sum;
+                prefix_sum.exec(gpu::WorkSize(workGroupSize, globalWorkSize), numbers_gpu, result_gpu, max_sum_gpu);
+                max_sum_gpu.readN(&max_sum, 1);
+
+                EXPECT_THE_SAME(reference_max_sum, max_sum, "CPU result should be consistent!");
+                t.nextLap();
+            }
+            std::cout << "GPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
+            std::cout << "GPU: " << (n / 1000.0 / 1000.0) / t.lapAvg() << " millions/s" << std::endl;
         }
     }
 }
