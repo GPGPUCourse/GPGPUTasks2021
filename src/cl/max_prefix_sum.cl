3d09f15385d5722008b75a8274195574622f05b4
ElectricR <chulkovroma@mail.ru>
2021-10-04 08:32:50 +0300

    Max prefix sum

--- src/cl/max_prefix_sum.cl
+++ src/cl/max_prefix_sum.cl
@@ -1,1 +1,37 @@
-// TODO
+__kernel void sum_reductor(__global int* data, unsigned read_offset, unsigned write_offset, unsigned current_reduction_size) {
+    if (get_global_id(0) >= current_reduction_size) return;
+    data[write_offset + get_global_id(0)] = data[read_offset + 2 * get_global_id(0)] + data[read_offset + 2 * get_global_id(0) + 1];
+}
+
+__kernel void sum_calculator(__global int* data, unsigned current_length) {
+    unsigned id = get_global_id(0);
+    if (id >= current_length) return;
+    unsigned item = id + 1;
+    unsigned current_binary_group_id = id;
+    unsigned current_mask = 1;
+    unsigned offset = 0;
+
+    while (current_mask <= item) {
+        if (current_mask == 1) {
+            if ((current_mask & item) == 0) {
+                data[id] = 0;
+            }
+        } else {
+            if (current_mask & item) {
+                data[id] += data[offset + current_binary_group_id];
+            }
+        }
+        current_binary_group_id /= 2;
+        current_binary_group_id = current_binary_group_id & 0b11111111111111111111111111111110;
+        offset += current_length;
+        current_length /= 2;
+        current_mask = current_mask << 1;
+    }
+}
+            
+__kernel void min_reductor(__global int* data, unsigned read_offset, unsigned write_offset) {
+    int a = data[read_offset + 2 * get_global_id(0)];
+    int b = data[read_offset + 2 * get_global_id(0) + 1];
+    data[write_offset + get_global_id(0)] = a > b ? a : b;
+}
+
--- src/main_max_prefix_sum.cpp
+++ src/main_max_prefix_sum.cpp
@@ -2,6 +2,13 @@
 #include <libutils/timer.h>
 #include <libutils/fast_random.h>
 
+#include <libgpu/context.h>
+#include <libgpu/shared_device_buffer.h>
+
+#include "cl/max_prefix_sum_cl.h"
+
+#include <numeric>
+#include <iterator>
 
 template<typename T>
 void raiseFail(const T &a, const T &b, std::string message, std::string filename, int line)
@@ -19,6 +26,21 @@ int main(int argc, char **argv)
 {
     int benchmarkingIters = 10;
     int max_n = (1 << 24);
+
+    gpu::Device device = gpu::chooseGPUDevice(argc, argv);
+    
+    gpu::Context context;
+    context.init(device.device_id_opencl);
+    context.activate();
+
+    ocl::Kernel sum_reductor(max_prefix_sum_kernel, max_prefix_sum_kernel_length, "sum_reductor");
+    sum_reductor.compile(true);
+
+    ocl::Kernel sum_calculator(max_prefix_sum_kernel, max_prefix_sum_kernel_length, "sum_calculator");
+    sum_calculator.compile(true);
+
+    ocl::Kernel min_reductor(max_prefix_sum_kernel, max_prefix_sum_kernel_length, "min_reductor");
+    min_reductor.compile(true);
 
     for (int n = 2; n <= max_n; n *= 2) {
         std::cout << "______________________________________________" << std::endl;
@@ -71,7 +93,50 @@ int main(int argc, char **argv)
         }
 
         {
-            // TODO: implement on OpenCL
+            const unsigned work_group_size = 128;
+
+            std::vector<int> xa(n * 2, 0); 
+            std::copy(as.begin(), as.end(), xa.begin());
+
+            timer t;
+            for (int iter = 0; iter < benchmarkingIters; ++iter) {
+
+                gpu::gpu_mem_32i buffer;
+                buffer.resizeN(xa.size() + 1); 
+                buffer.writeN(xa.data(), xa.size()); 
+
+                unsigned current_reduction_size = static_cast<unsigned>(xa.size()) / 4;
+                unsigned read_offset = 0;
+                unsigned write_offset = current_reduction_size * 2;
+
+                while (current_reduction_size) {
+                    sum_reductor.exec(gpu::WorkSize(work_group_size, current_reduction_size), buffer, read_offset, write_offset, current_reduction_size);
+                    read_offset = write_offset;
+                    write_offset += current_reduction_size;
+                    current_reduction_size /= 2;
+                }
+                
+                sum_calculator.exec(gpu::WorkSize(128, xa.size() / 2), buffer, static_cast<unsigned>(xa.size()) / 2);
+
+                current_reduction_size = static_cast<unsigned>(xa.size()) / 4;
+                read_offset = 0;
+                write_offset = current_reduction_size * 2;
+
+                while (current_reduction_size) {
+                    min_reductor.exec(gpu::WorkSize(work_group_size, current_reduction_size), buffer, read_offset, write_offset);
+                    read_offset = write_offset;
+                    write_offset += current_reduction_size;
+                    current_reduction_size /= 2;
+                }
+                
+                int max_sum = 0;
+                buffer.readN(&max_sum, 1, xa.size() - 2);
+                max_sum = std::max(max_sum, 0);
+                EXPECT_THE_SAME(reference_max_sum, max_sum, "CPU result should be consistent!");
+                t.nextLap();
+            }
+            std::cout << "GPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
+            std::cout << "GPU: " << (n / 1000.0 / 1000.0) / t.lapAvg() << " millions/s" << std::endl;
         }
     }
 }
