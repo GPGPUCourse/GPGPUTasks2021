b3bd43146b57e06fe010e031d616ac1ce3fcf14a
Alexander Valiullin <phoenix-asv@yandex.ru>
2021-10-08 16:52:34 +0300

    task04: matrix transpose: naive

--- src/cl/matrix_transpose.cl
+++ src/cl/matrix_transpose.cl
@@ -1,4 +1,13 @@
-__kernel void matrix_transpose(...)
+__kernel void matrix_transpose(__global const float* inp, __global float* out, unsigned int nr, unsigned int nc)
 {
-    // TODO
+    // Work-item index info
+    const unsigned int lr = get_local_id(0);
+    const unsigned int gr = get_global_id(0);
+    const unsigned int lc = get_local_id(1);
+    const unsigned int gc = get_global_id(1);
+
+    // Naive transpose
+    if (gr < nr && gc < nc) {
+        out[gc * nr + gr] = inp[gr * nc + gc];
+    }
 }
--- src/main_matrix_transpose.cpp
+++ src/main_matrix_transpose.cpp
@@ -20,19 +20,18 @@ int main(int argc, char **argv)
     context.activate();
 
     int benchmarkingIters = 10;
-    unsigned int M = 1024;
-    unsigned int K = 1024;
+    unsigned int M = 1024; // number of rows
+    unsigned int K = 2048; // number of columns
 
     std::vector<float> as(M*K, 0);
-    std::vector<float> as_t(M*K, 0);
+    std::vector<float> as_t(K*M, 0);
 
-    FastRandom r(M+K);
-    for (unsigned int i = 0; i < as.size(); ++i) {
-        as[i] = r.nextf();
+    FastRandom random(M+K);
+    for (float & a : as) {
+        a = random.nextf();
     }
     std::cout << "Data generated for M=" << M << ", K=" << K << "!" << std::endl;
 
-    /*
     gpu::gpu_mem_32f as_gpu, as_t_gpu;
     as_gpu.resizeN(M*K);
     as_t_gpu.resizeN(K*M);
@@ -45,23 +44,21 @@ int main(int argc, char **argv)
     {
         timer t;
         for (int iter = 0; iter < benchmarkingIters; ++iter) {
-            // TODO
-            unsigned int work_group_size = 128;
-            unsigned int global_work_size = ...;
             // Для этой задачи естественнее использовать двухмерный NDRange. Чтобы это сформулировать
             // в терминологии библиотеки - нужно вызвать другую вариацию конструктора WorkSize.
             // В CLion удобно смотреть какие есть вариант аргументов в конструкторах:
             // поставьте каретку редактирования кода внутри скобок конструктора WorkSize -> Ctrl+P -> заметьте что есть 2, 4 и 6 параметров
             // - для 1D, 2D и 3D рабочего пространства соответственно
-            matrix_transpose_kernel.exec(gpu::WorkSize(work_group_size, global_work_size), as_gpu, as_t_gpu, M, K);
-
+            unsigned int work_group_size = 16;
+            auto work_size = gpu::WorkSize(work_group_size, work_group_size, M, K);
+            matrix_transpose_kernel.exec(work_size, as_gpu, as_t_gpu, M, K);
             t.nextLap();
         }
         std::cout << "GPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
         std::cout << "GPU: " << M*K/1000.0/1000.0 / t.lapAvg() << " millions/s" << std::endl;
     }
 
-    as_t_gpu.readN(as_t.data(), M*K);
+    as_t_gpu.readN(as_t.data(), K*M);
 
     // Проверяем корректность результатов
     for (int j = 0; j < M; ++j) {
@@ -74,7 +71,6 @@ int main(int argc, char **argv)
             }
         }
     }
-    */
 
     return 0;
 }
