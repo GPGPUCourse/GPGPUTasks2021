f30e422d70d8ce2e425f4cffa2d995c696f775c3
ElectricR <chulkovroma@mail.ru>
2021-10-25 17:30:05 +0300

    Partial radix sort

--- src/cl/radix.cl
+++ src/cl/radix.cl
@@ -1,3 +1,59 @@
-__kernel void radix(__global unsigned int *as) {
-    // TODO
+#define k 2
+
+__kernel void calc_c_init(__global unsigned int *as, __global unsigned *cs, unsigned n, unsigned stage) {
+    __local unsigned local_cs[1 << k];
+    local_cs[get_local_id(0)] = 0;
+
+    unsigned mask = ((0xffffffff << (32 - k)) >> (32 - k)) << (stage * k);
+    unsigned curr_val = (as[get_global_id(0)] & mask) >> (k * stage);
+    atomic_add(local_cs + curr_val, 1);
+
+    barrier(CLK_LOCAL_MEM_FENCE);
+    if (get_local_id(0) / (1 << k) == 0) {
+        unsigned offset = get_local_id(0) % (1 << k);
+        cs[(1 << k) * get_group_id(0) + offset] = local_cs[offset];
+    }
+}
+
+__kernel void calc_c_tree(__global int* cs, unsigned read_offset, unsigned write_offset, unsigned current_reduction_size) {
+    if (get_global_id(0) >= current_reduction_size) return;
+    cs[write_offset + get_global_id(0)] = cs[read_offset + 2 * get_global_id(0)] + cs[read_offset + 2 * get_global_id(0) + 1];
+}
+
+__kernel void matrix_transpose(__global float* data, __global float* output, unsigned w, unsigned h, unsigned work_group_size)
+{
+    __local float mem[WARP_SIZE][WARP_SIZE];
+
+    for (int i = 0; i != WARP_SIZE * WARP_SIZE / work_group_size; ++i) {
+        unsigned mem_y = get_local_id(1) + i * get_local_size(1);
+        unsigned mem_x = (get_local_id(0) + mem_y) % WARP_SIZE; //
+
+        unsigned in_x = get_global_id(0);
+        unsigned in_y = get_group_id(1) * WARP_SIZE + i * get_local_size(1) + get_local_id(1);
+
+        unsigned source_index = in_x + in_y * w; 
+        if (in_x < w && in_y < h) {
+            mem[mem_x][mem_y] = data[source_index];
+        } else {
+            mem[mem_x][mem_y] = 0;
+        }
+    }
+
+    barrier(CLK_LOCAL_MEM_FENCE);   
+
+    unsigned tile_x_shift = get_group_id(1) * WARP_SIZE;
+    unsigned tile_y_shift = get_group_id(0) * WARP_SIZE;
+
+    for (int i = 0; i != WARP_SIZE * WARP_SIZE / work_group_size; ++i) {
+        unsigned out_x = tile_x_shift + get_local_id(0);
+        unsigned out_y = tile_y_shift + get_local_id(1) + i * get_local_size(1);
+
+        unsigned mem_y = get_local_id(0);
+        unsigned mem_x = (get_local_id(1) + get_local_size(1) * i + mem_y) % WARP_SIZE;
+
+        unsigned output_index = out_x + out_y * h; 
+        if (out_x < h && out_y < w) {
+            output[output_index] = mem[mem_x][mem_y];
+        }
+    }
 }
--- src/main_radix.cpp
+++ src/main_radix.cpp
@@ -10,6 +10,7 @@
 #include <iostream>
 #include <stdexcept>
 #include <vector>
+#include <iterator>
 
 
 template<typename T>
@@ -22,6 +23,17 @@ void raiseFail(const T &a, const T &b, std::string message, std::string filename
 
 #define EXPECT_THE_SAME(a, b, message) raiseFail(a, b, message, __FILE__, __LINE__)
 
+void calc_c_tree(ocl::Kernel& calc_c_tree_kernel, gpu::gpu_mem_32u& c_tree_gpu, unsigned c_size, unsigned work_group_size) {
+    unsigned current_reduction_size = c_size / 2;
+    unsigned read_offset = 0;
+    unsigned write_offset = current_reduction_size * 2;
+    while (current_reduction_size) {
+        calc_c_tree_kernel.exec(gpu::WorkSize(work_group_size, current_reduction_size), c_tree_gpu, read_offset, write_offset, current_reduction_size);
+        read_offset = write_offset;
+        write_offset += current_reduction_size;
+        current_reduction_size /= 2;
+    }
+}
 
 int main(int argc, char **argv) {
     gpu::Device device = gpu::chooseGPUDevice(argc, argv);
@@ -30,8 +42,9 @@ int main(int argc, char **argv) {
     context.init(device.device_id_opencl);
     context.activate();
 
-    int benchmarkingIters = 10;
-    unsigned int n = 32 * 1024 * 1024;
+    int benchmarkingIters = 1;
+    unsigned int n = 64;
+
     std::vector<unsigned int> as(n, 0);
     FastRandom r(n);
     for (unsigned int i = 0; i < n; ++i) {
@@ -48,15 +61,34 @@ int main(int argc, char **argv) {
             t.nextLap();
         }
         std::cout << "CPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
-        std::cout << "CPU: " << (n / 1000 / 1000) / t.lapAvg() << " millions/s" << std::endl;
+        std::cout << "CPU: " << (static_cast<double>(n) / 1000 / 1000) / t.lapAvg() << " millions/s" << std::endl;
     }
-    /*
     gpu::gpu_mem_32u as_gpu;
     as_gpu.resizeN(n);
 
+    const unsigned int workGroupSize = 32;
+    const unsigned int global_work_size = (n + workGroupSize - 1) / workGroupSize * workGroupSize;
+    const unsigned k = 2;
+    unsigned int c_size = n / workGroupSize * (1 << k);
+    unsigned c_tree_size = c_size * 2;
+
+    as = {1, 0, 1, 2, 5, 1, 0, 0, 3, 2, 4, 7, 1, 2, 1, 1, 1, 0, 1, 2, 5, 1, 0, 0, 3, 2, 4, 7, 1, 2, 1, 1, 1, 0, 1, 2, 5, 1, 0, 0, 3, 2, 4, 7, 1, 2, 1, 1, 1, 0, 1, 2, 5, 1, 0, 0, 3, 2, 4, 7, 1, 2, 1, 1};
+
+    gpu::gpu_mem_32u c_init_gpu;
+    c_init_gpu.resizeN(c_size);
+
+    gpu::gpu_mem_32u c_tree_gpu;
+    c_tree_gpu.resizeN(c_tree_size);
+
     {
-        ocl::Kernel radix(radix_kernel, radix_kernel_length, "radix");
-        radix.compile();
+        ocl::Kernel calc_c_init(radix_kernel, radix_kernel_length, "calc_c_init");
+        calc_c_init.compile();
+
+        ocl::Kernel tranpose_init_c_kernel(radix_kernel, radix_kernel_length, "matrix_transpose");
+        tranpose_init_c_kernel.compile();
+
+        ocl::Kernel calc_c_tree_kernel(radix_kernel, radix_kernel_length, "calc_c_tree");
+        calc_c_tree_kernel.compile();
 
         timer t;
         for (int iter = 0; iter < benchmarkingIters; ++iter) {
@@ -64,21 +96,25 @@ int main(int argc, char **argv) {
 
             t.restart();// Запускаем секундомер после прогрузки данных, чтобы замерять время работы кернела, а не трансфер данных
 
-            unsigned int workGroupSize = 128;
-            unsigned int global_work_size = (n + workGroupSize - 1) / workGroupSize * workGroupSize;
-            radix.exec(gpu::WorkSize(workGroupSize, global_work_size), as_gpu, n);
+            calc_c_init.exec(gpu::WorkSize(workGroupSize, global_work_size), as_gpu, c_init_gpu, n, 0);
+            tranpose_init_c_kernel.exec(gpu::WorkSize(workGroupSize, global_work_size), c_init_gpu, c_tree_gpu, (1 << k), c_size / (1 << k), workGroupSize);
+
+            calc_c_tree(calc_c_tree_kernel, c_tree_gpu, c_size, workGroupSize);
+            // radix.exec(gpu::WorkSize(workGroupSize, global_work_size), as_gpu, n);
             t.nextLap();
         }
         std::cout << "GPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
-        std::cout << "GPU: " << (n / 1000 / 1000) / t.lapAvg() << " millions/s" << std::endl;
+        std::cout << "GPU: " << (static_cast<double>(n) / 1000 / 1000) / t.lapAvg() << " millions/s" << std::endl;
 
         as_gpu.readN(as.data(), n);
     }
+    c_tree_gpu.readN(as.data(), c_tree_size);
+    std::copy(as.begin(), as.begin() + c_tree_size, std::ostream_iterator<unsigned>(std::cout, " "));
+    std::cout << std::endl;
 
     // Проверяем корректность результатов
     for (int i = 0; i < n; ++i) {
         EXPECT_THE_SAME(as[i], cpu_sorted[i], "GPU results should be equal to CPU results!");
     }
-*/
     return 0;
 }
