be8bca2fc66c76dbbf94a4d1b18a73b79fdab2f4
lissrbay <rbayliss964@gmail.com>
2021-11-08 17:04:23 +0300

    add merge sort

--- src/cl/merge.cl
+++ src/cl/merge.cl
@@ -1,1 +1,36 @@
+#ifdef __CLION_IDE__
+    #include <libgpu/opencl/cl/clion_defines.cl>
+#endif
 
+#line 6
+
+__kernel void merge(__global float* a, __global float* b, unsigned int n, unsigned int i) {
+    unsigned int ind = get_global_id(0);
+
+    if (ind >= n) {
+        return;
+    }
+
+    unsigned int a_begin = ind / (i * 2) * (i * 2);
+
+    if (a_begin >= n){
+        return;
+    }
+
+    unsigned int a_end = min(a_begin + i, n);
+    unsigned int b_begin = a_end;
+    unsigned int b_end = min(b_begin + i, n);
+
+    int l = ind < a_end ? a_end : a_begin;
+    int r = ind < a_end ? b_end : a_end;
+    while (l < r) {
+        int m = (l + r) / 2;
+        if ((a[m] < a[ind]) || (m < b_begin && a[m] == a[ind])) {
+            l = m + 1;
+        } else {
+            r = m;
+        }
+    }
+    
+    b[ind + l - b_begin] = a[ind];
+}
--- src/main_merge.cpp
+++ src/main_merge.cpp
@@ -14,7 +14,7 @@
 
 template<typename T>
 void raiseFail(const T &a, const T &b, std::string message, std::string filename, int line) {
-    if (a != b) {
+    if (abs(a - b) > 1e-6) {
         std::cerr << message << " But " << a << " != " << b << ", " << filename << ":" << line << std::endl;
         throw std::runtime_error(message);
     }
@@ -33,9 +33,12 @@ int main(int argc, char **argv) {
     int benchmarkingIters = 10;
     unsigned int n = 32 * 1024 * 1024;
     std::vector<float> as(n, 0);
+    std::vector<float> bs(n, 0);
+
     FastRandom r(n);
     for (unsigned int i = 0; i < n; ++i) {
         as[i] = r.nextf();
+        bs[i] = 0.0;
     }
     std::cout << "Data generated for n=" << n << "!" << std::endl;
 
@@ -50,29 +53,38 @@ int main(int argc, char **argv) {
         std::cout << "CPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
         std::cout << "CPU: " << (n / 1000 / 1000) / t.lapAvg() << " millions/s" << std::endl;
     }
-    /*
+
     gpu::gpu_mem_32f as_gpu;
+    gpu::gpu_mem_32f bs_gpu;
     as_gpu.resizeN(n);
+    bs_gpu.resizeN(n);
+
     {
         ocl::Kernel merge(merge_kernel, merge_kernel_length, "merge");
         merge.compile();
         timer t;
         for (int iter = 0; iter < benchmarkingIters; ++iter) {
             as_gpu.writeN(as.data(), n);
+            bs_gpu.writeN(bs.data(), n);
+
             t.restart();// Запускаем секундомер после прогрузки данных, чтобы замерять время работы кернела, а не трансфера данных
             unsigned int workGroupSize = 128;
             unsigned int global_work_size = (n + workGroupSize - 1) / workGroupSize * workGroupSize;
-            merge.exec(gpu::WorkSize(workGroupSize, global_work_size), as_gpu, n);
+            for (unsigned int i = 1; i < n; i <<= 1) {
+                merge.exec(gpu::WorkSize(workGroupSize, global_work_size), as_gpu, bs_gpu, n, i);
+                as_gpu.swap(bs_gpu);
+            }
             t.nextLap();
         }
         std::cout << "GPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
         std::cout << "GPU: " << (n / 1000 / 1000) / t.lapAvg() << " millions/s" << std::endl;
-        as_gpu.readN(as.data(), n);
+        as_gpu.readN(bs.data(), n);
     }
     // Проверяем корректность результатов
     for (int i = 0; i < n; ++i) {
-        EXPECT_THE_SAME(as[i], cpu_sorted[i], "GPU results should be equal to CPU results!");
+        //std::cout << cs[i] << " " << cpu_sorted[i] << std::endl;
+        EXPECT_THE_SAME(bs[i], cpu_sorted[i], "GPU results should be equal to CPU results!");
     }
-*/
+
     return 0;
 }
