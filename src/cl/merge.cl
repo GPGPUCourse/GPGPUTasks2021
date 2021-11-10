4a71e5b812f141ac365741980012b6ac326b7af1
Vasily Ionin <ionin.code@gmail.com>
2021-11-10 05:46:38 +0300

    Add solution

--- src/cl/merge.cl
+++ src/cl/merge.cl
@@ -1,1 +1,60 @@
+#ifdef __CLION_IDE__
 
+#include <libgpu/opencl/cl/clion_defines.cl>
+
+#endif
+
+#line 6
+
+__kernel void merge(__global float *as, unsigned int size, __global float *out, unsigned int block_size) {
+    const unsigned int index = get_global_id(0);
+    if (index >= size) {
+        return;
+    }
+
+    const unsigned int merge_interval = 2 * block_size;
+    const unsigned int l = (index / merge_interval) * merge_interval;
+    const unsigned int r = l + block_size;
+    const unsigned int end = min(r + block_size, size);
+    // left block: [l, r); right block: [r, end)
+    // internally blocks are sorted already
+
+    const float current = as[index];
+    if (r >= size) {
+        out[index] = current; // merge is identity on the last block
+        return;
+    }
+
+    unsigned int from, to;
+    if (index < r) { // index is in the left block
+        from = r;
+        to = end;
+    } else {         // index is in the right block
+        from = l;
+        to = r;
+    }
+    // [from, to) is the opposite block, i.e. block which does not contain index
+
+    while (from < to) {
+        const unsigned int mid = (to - from) / 2 + from;
+        if (as[mid] < current) {
+            from = mid + 1;
+        } else {
+            to = mid;
+        }
+    }
+    const unsigned int pivot = from; // smallest index: as[pivot] >= current
+    unsigned int next = pivot;       // smallest index: as[pivot] > current
+    for (; current == as[next] && next < r; ++next) {}
+
+    // currently: as[i] < current on [l, pivot), as[i] == current on [pivot, next), as[i] > current on [next, r)
+    unsigned int count = 0; // how many elements were already merged in our group
+    if (index < r) {
+        // index is in the left block: l <= i < r; r <= p <= n < end
+        count = (index - l) + (pivot - r);
+    } else {
+        // index is in the right block: l <= p <= n < r; r <= index < end
+        count = (next - l) + (index - r);
+    }
+    out[l + count] = current;
+}
--- src/main_merge.cpp
+++ src/main_merge.cpp
@@ -44,15 +44,19 @@ int main(int argc, char **argv) {
         timer t;
         for (int iter = 0; iter < benchmarkingIters; ++iter) {
             cpu_sorted = as;
+            t.restart();
             std::sort(cpu_sorted.begin(), cpu_sorted.end());
             t.nextLap();
         }
         std::cout << "CPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
-        std::cout << "CPU: " << (n / 1000 / 1000) / t.lapAvg() << " millions/s" << std::endl;
+        std::cout << "CPU: " << (n / 1000. / 1000.) / t.lapAvg() << " millions/s" << std::endl;
     }
-    /*
+
     gpu::gpu_mem_32f as_gpu;
     as_gpu.resizeN(n);
+
+    gpu::gpu_mem_32f out_gpu;
+    out_gpu.resizeN(n);
     {
         ocl::Kernel merge(merge_kernel, merge_kernel_length, "merge");
         merge.compile();
@@ -62,17 +66,21 @@ int main(int argc, char **argv) {
             t.restart();// Запускаем секундомер после прогрузки данных, чтобы замерять время работы кернела, а не трансфера данных
             unsigned int workGroupSize = 128;
             unsigned int global_work_size = (n + workGroupSize - 1) / workGroupSize * workGroupSize;
-            merge.exec(gpu::WorkSize(workGroupSize, global_work_size), as_gpu, n);
+            for (unsigned int depth = 0, block_size = 1; block_size < n; ++depth, block_size *= 2) {
+                // merge_interval = 2 * block_size >= n on the last iteration
+                merge.exec(gpu::WorkSize(workGroupSize, global_work_size), as_gpu, n, out_gpu, block_size);
+                out_gpu.swap(as_gpu);
+            }
             t.nextLap();
         }
         std::cout << "GPU: " << t.lapAvg() << "+-" << t.lapStd() << " s" << std::endl;
-        std::cout << "GPU: " << (n / 1000 / 1000) / t.lapAvg() << " millions/s" << std::endl;
+        std::cout << "GPU: " << (n / 1000. / 1000.) / t.lapAvg() << " millions/s" << std::endl;
         as_gpu.readN(as.data(), n);
     }
     // Проверяем корректность результатов
     for (int i = 0; i < n; ++i) {
         EXPECT_THE_SAME(as[i], cpu_sorted[i], "GPU results should be equal to CPU results!");
     }
-*/
+
     return 0;
 }
